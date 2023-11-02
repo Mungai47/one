ec9da3725d19f80ac5606f6563751b14001471e0
Joseph <Josephmburumungai99@gmail.com>
2023-11-02 22:40:14 +0300

    Created the Hero component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,12 @@ function Header() {
 
     const [nav, setNav] =useState(false);
 
+    const closeMenu = () => {
+        setNav(false);
+    }
+
   return (
-    <header className="bg-[#141c27] text-white shadow-lg">
+    <header className="bg-[#141c27] text-white shadow-lg fixed w-full z-10">
         <div className="flex items-center justify-between p-4 w-4/5 mx-auto">
             <div className="text-2xl lg:text-3xl font-bold">
                 <Link to="/" >
@@ -41,19 +45,19 @@ function Header() {
 
             {nav && (
                 <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen font-semibold text-4xl cursor-pointer bg-gradient-to-b from-black to-gray-800">
-                   <Link to="/" >
+                   <Link to="/" onClick={closeMenu}>
                       <li className="hover:underline px-4 py-6">Home</li>
                    </Link>
-                   <Link to="/portfolio" >
+                   <Link to="/portfolio" onClick={closeMenu}>
                       <li className="hover: px-4 py-6">Porfolio</li>
                    </Link>
-                   <Link to="/services" >
+                   <Link to="/services" onClick={closeMenu}>
                       <li className="hover:underline px-4 py-6">Services</li>
                    </Link>
-                   <Link to="/contacts" >
+                   <Link to="/contacts" onClick={closeMenu}>
                       <li className="hover:underline px-4 py-6">Contacts</li>
                    </Link>
-                   <Link to="/contacts" >
+                   <Link to="/contacts" onClick={closeMenu}>
                        <button className="bg-green text-black px-5 py-1 rounded-full hover:opacity-80">Hire Me</button>
                    </Link>
                 </ul>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import { BiDownload } from 'react-icons/bi'
+import { AiFillPlayCircle } from 'react-icons/ai'
+
+function Hero() {
+    return (
+        <div className="bg-[#09101a] text-white h-screen flex items-center">
+            <div className="w-4/5 mx-auto flex items-center gap-10">               
+                <div className="lg:w-2/3 lg:p-4">  
+                    <h2 className="uppercase py-4 text-4xl md:text-4xl lg:text-6xl font-bold">
+                        Hi, I&rsquo;m Mungai 
+                        <br />
+                        A <span className="text-green">full-stack web Developer</span>
+                    </h2>
+                    <p className="text-[#94a3b8] pb-4 font-medium">
+                    I am a full stack web developer with expertise in both front-end and back-end technologies. I design and implement web applications that seamlessly combine the user interface with the server-side logic. I take pride in my ability to tackle a project from concept to deployment, collaborating with teams to create dynamic, efficient, and secure web solutions that meet the needs of clients and users alike.
+                    </p>
+                    <div className='flex items-center gap-6 lg:gap-10 p-5'>
+                        <button className="bg-green text-black px-5 py-3 rounded-xl text-lg font-semibold hover:opacity-80 flex items-center gap-2">
+                            Download Cv 
+                            <BiDownload size={30}/>
+                        </button>
+                        <div className="flex items-center gap-3 cursor-pointer">
+                            <AiFillPlayCircle size={30} className="text-green"/>
+                            <p className='text-base md:text-lg font-semibold hover:text-green'>Watch The Video</p>
+                        </div>
+                    </div>
+                </div>                    
+                <div className='hidden lg:block relative'>
+                    <img src="/public/images/image1.png" alt="Profile Image" className="absolute z-10 " />
+                    <div className="w-64 h-96 bg-gray-700 bg-green rotate-12"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
